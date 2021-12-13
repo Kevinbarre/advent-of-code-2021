c3ec8d4669a3c1161f66a741e04c6c62b838fca7
Kévin Barré <kevinbarre90@gmail.com>
2021-12-13 23:59:03 +0100

    Day 11 - Part 1

---
+++ Day11/part1.py
@@ -0,0 +1,56 @@
+import sys
+from pprint import pprint
+
+
+def flash(k, l, state, flashings):
+    state[k][l] += 1
+    if state[k][l] == 10:
+        flashings.append((k, l))
+
+
+def step(state):
+    # First increment everything by 1, and check if it's flashing
+    flashings = []
+    for i in range(10):
+        for j in range(10):
+            state[i][j] += 1
+            if state[i][j] == 10:
+                flashings.append((i, j))
+
+    # Flashing octopus propagate to their neighbours
+    while flashings:
+        i, j = flashings.pop()
+        # Increment flashing neighbours by 1
+        for k in range(i - 1, i + 2):
+            for l in range(j - 1, j + 2):
+                if k == i and l == j:
+                    # Ignore current cell
+                    continue
+                if 0 <= k < 10 and 0 <= l < 10:  # Only flash valid cells
+                    flash(k, l, state, flashings)
+
+    # Count number of flashs, and reset them to 0
+    nb_flash = 0
+    for i in range(10):
+        for j in range(10):
+            if state[i][j] >= 10:
+                state[i][j] = 0
+                nb_flash += 1
+
+    return nb_flash
+
+
+octopuses = []
+for line in sys.stdin:
+    octopuses.append(list(map(int, line.rstrip('\n'))))
+
+total_flashes = 0
+print("Before any steps:")
+pprint(octopuses)
+
+for s in range(1, 101):
+    total_flashes += step(octopuses)
+    print("After step {}:".format(s))
+    pprint(octopuses)
+
+print("Total flashes:", total_flashes)
